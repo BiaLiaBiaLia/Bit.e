96e1d56bf4b4f5d49bd9b44248a4909405646b08
BiaLiaBiaLia <1343503640@qq.com>
2019-11-10 19:40:58 +0800

    daily exercise

--- 20191110/20191110/20191110.cpp
+++ 20191110/20191110/20191110.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <string>
 #include <algorithm>
 using namespace std;
@@ -7,32 +6,33 @@ using namespace std;
 #if 0
 int main()
 {
-	//十进制转其他进制(2<=N<=16)
-	int M, N;
-	cin >> M >> N;
+	//由于N的取值范围是2~16
 	string ret;
-	string str{ "0123456789ABCDEF" };
+	string table("0123456789ABCDEF");
 
-	int flag = 1;//默认输入的数为正数
-	if (M < 0)
+	int M;
+	int N;
+	cin >> M >> N;
+
+	int flag = 1;//默认是正数
+	if (M < 0)//假如是负数
 	{
 		flag = 0;
-		M = -M;//处理为正数
+		M = -M;
 	}
 
 	while (M)
 	{
-		ret += str[M%N];
+		ret += table[M%N];
 		M /= N;
 	}
 
-	if (flag == 0)
+	if (flag == 0)//是负数
 	{
 		ret += '-';
 	}
-
+	//得反转一下字符串(之前是从后到前倒着存的)
 	reverse(ret.begin(), ret.end());
-
 	cout << ret << endl;
 
 	system("pause");
@@ -40,26 +40,19 @@ int main()
 }
 #endif
 
-/*输入两个字符串，从第一字符串中删除第二个字符串中所有的字符。
-例如，输入”They are students.”和”aeiou”，
-则删除之后的第一个字符串变成”Thy r stdnts.”*/
-
 int main()
 {
 	string str1, str2;
+
 	getline(cin, str1);
 	getline(cin, str2);
 
-	// 使用哈希映射思想先统计str2字符出现的次数
 	int hashtable[256] = { 0 };
 	for (int i = 0; i < str2.size(); ++i)
 	{
 		hashtable[str2[i]]++;
 	}
 
-// 遍历str1，str1[i]映射hashtable对应位置为0，则表示这个字符在
-// str2中没有出现过，则将他+=到ret。注意这里最好不要str1.erases(i)
-// 因为边遍历，边erase，容易出错。
 	string ret;
 	for (int i = 0; i < str1.size(); ++i)
 	{
