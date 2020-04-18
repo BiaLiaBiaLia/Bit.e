6e01b23e179e43f0c9c68e802df9e6551a1f0df1
BiaLiaBiaLia <1343503640@qq.com>
2020-04-18 22:57:01 +0800

    daily exercise

---
+++ 20200418/20200418/20200418.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+//Æ´´ÕÕý·½ÐÎ
+int main() {
+	vector<int> input(4);
+
+	for (int i = 0; i < 4; ++i)
+		cin >> input[i];
+	sort(input.begin(), input.end());
+
+	int res = 0, tmp = 0;
+	for (int i = 0; i < 4; ++i) {
+		tmp += abs(input[i] - input[1]);
+	}
+
+	res = tmp;
+	tmp = 0;
+	for (int i = 0; i < 4; ++i) {
+		tmp += abs(input[i] - input[2]);
+	}
+
+	res = min(tmp, res);
+	cout << res << endl;
+
+	system("pause");
+	return 0;
+}
